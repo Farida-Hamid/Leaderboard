294dc0471b72d16741f2756705a22c5901c59281
Farida-Hamid <faridaaddala@gmail.com>
2022-06-22 17:04:36 +0300

    Build event to call the post function

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import { fetchScores } from './modules/display-scores.js';
 import { sendScore } from './modules/add-score.js';
 
 const refresh = document.querySelector('#refresh');
+const scoreAdder = document.getElementById('submit');
 
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/45ygaw4er3w4rt34ty64yrtrw3rik8/scores/`;
 
 refresh.onclick = (e) => {
   e.preventDefault();
+  fetchScores(url);
+};
+
+scoreAdder.onclick = (e) => {
+  e.preventDefault();
+  sendScore(url);
   fetchScores(url);
 };
