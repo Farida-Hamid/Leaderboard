381a84a5fbcf8be86eb79e825890b9fe7c1e13c0
Farida-Hamid <faridaaddala@gmail.com>
2022-06-22 11:19:43 +0300

    Add URL fo API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './style.css';
 import fetchScores from './modules/display-scores.js';
 
 const refresh = document.querySelector('#refresh');
-const url = ``;
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/45ygaw4er3w4rt34ty64yrtrw3rik8/scores/`;
 
 refresh.addEventListener('click', fetchScores(url));
