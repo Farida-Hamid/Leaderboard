e9c72ae101526aec54db2c5420e09739bc9f4749
Farida-Hamid <faridaaddala@gmail.com>
2022-06-22 17:05:24 +0300

    Build  post function

--- src/modules/add-score.js
+++ src/modules/add-score.js
@@ -1,5 +1,22 @@
 const formData = document.querySelector('#add');
 
 export const sendScore = async (url) => {
+  try {
+    const message = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: formData.name.value,
+        score: formData.score.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
 
+    return message.json();
+  }
+   catch (error) {
+    console.log(error);
+    return false;
+  }
 };
